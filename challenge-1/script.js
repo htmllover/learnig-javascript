986afb9634566cd7ba39b159f40ab986bb5598aa
Aminul Islam <aminul@divine-it.net>
2019-01-20 13:01:20 +0600

    update challenge 1

--- challenge-1/script.js
+++ challenge-1/script.js
@@ -1,1 +1,11 @@
-var johnMass = 68;
+var johnMass = 250;
+var johnHeight = 10;
+var markMass = 220;
+var markHeight = 12;
+
+var johnBmi = johnMass / ( johnHeight * johnHeight);
+var markBmi = markMass / ( markHeight * markHeight); 
+
+var bmiResult = johnBmi <= markBmi;
+
+document.getElementById('demo').innerHTML = ("Is Mark's  higher than John's?" + ' ' + bmiResult); 
